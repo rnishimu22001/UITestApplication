7f49bb2776c662280c1a91b05c101c336dff63cb
rnishimu22001 <rnishimu22001@yahoo.co.jp>
2019-10-14 13:18:31 +0900

    migrate swift5

--- UITestApplicationUITests/UITestApplicationTableUITests.swift
+++ UITestApplicationUITests/UITestApplicationTableUITests.swift
@@ -21,14 +21,23 @@ class UITestApplicationTableUITests: XCTestCase {
     func testTableViewCellExist() {
         
         let cells = self.app.cells.matching(identifier: "TestCell")
-        let rowCount = 6
+        let firstIndex = 0
+        let lastIndex = 5
+        let rowCount = firstIndex...lastIndex
         
-        for i in 0..<rowCount {
-            let cell = cells.element(boundBy: i)
-            let label = cell.staticTexts[i.description]
-            // test will be failed
-            XCTAssertTrue(label.exists, "row number:" + i.description)
+        rowCount.forEach {
+            let cell = cells.element(boundBy: $0)
+            let label = cell.staticTexts[$0.description]
+            switch $0 {
+            case firstIndex...4:
+                // Found
+                XCTAssertTrue(label.exists, "row number:" + $0.description)
+            case lastIndex...lastIndex:
+                // Cell not found
+                XCTAssertFalse(label.exists, "row number:" + $0.description)
+            default:
+                XCTFail()
+            }
         }
-        
     }
 }
